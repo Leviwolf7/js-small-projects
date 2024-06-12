377dba74f4aefa3939f6f638063835609331e6a9
Isai Torres <142456446+Leviwolf7@users.noreply.github.com>
2024-06-12 14:51:03 -0400

    added js-dom-manipulation

---
+++ js-dom-manipulation/app.js
@@ -0,0 +1,7 @@
+/* This repository is for practicing DOM manipulation. Feel free to uncomment
+all the examples and see its functions in the console: */
+
+// 1. .getElementById(); --> returns the element with the specified ID.
+
+/*const container = document.getElementById('container-ingredients');
+console.log(container);*/
